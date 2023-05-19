85788493272e770dcdaeb269080df058e1192458
timothy-lew <timothyjblew2000@gmail.com>
2023-05-19 14:39:50 +0800

    remove negative bus time

--- src/components/Buses.js
+++ src/components/Buses.js
@@ -15,7 +15,15 @@ function Buses({ buses }) {
           <div key={bus.ServiceNo} className={styles.item}>
             <h1>{bus.ServiceNo} </h1>
             <p className={styles.p}>
-              {!isNaN(m1) ? Math.round((m1 - minutes) / 1000 / 60) : 'NA'}
+              {/* 
+                check if NAN
+                if < 0, display 0
+              */}
+              {isNaN(m1)
+                ? 'NA'
+                : Math.round((m1 - minutes) / 1000 / 60) < 0
+                ? 0
+                : Math.round((m1 - minutes) / 1000 / 60)}
             </p>
             <p className={styles.p}>
               {!isNaN(m2) ? Math.round((m2 - minutes) / 1000 / 60) : 'NA'}
